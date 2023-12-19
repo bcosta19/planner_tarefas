f8dbd9cff0b4f46c13754fcf28306d4991154190
brunocosta1 <brunocosta.cg1998@gmail.com>
2023-12-19 16:22:28 -0300

    Implementando login

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'package:planner_tarefas/Login.dart';
-import 'package:planner_tarefas/DashBoard.dart';
-import 'Tasks.dart';
-import 'Search.dart';
-import 'RecentsTasks.dart';
-import 'CompleteTasks.dart';
+import 'package:planner_tarefas/pages/Login.dart';
+import 'package:planner_tarefas/pages/DashBoard.dart';
+import 'pages/Tasks.dart';
+import 'pages/Search.dart';
+import 'pages/RecentsTasks.dart';
+import 'pages/CompleteTasks.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main(){
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(MaterialApp(
     title: "Planner",
     initialRoute: '/login',
---
+++ lib/pages/Login.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:planner_tarefas/controller/LoginController.dart';
+import '../model/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:planner_tarefas/pages/DashBoard.dart';
+
+class Login extends StatefulWidget{
+
+  const Login({super.key});
+  static const String routeName = "/login";
+
+  String getRouteName(){
+    return routeName;
+  }
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+enum LoginStatus {notSignIn, signIn}
+
+class _LoginState extends State<Login>{
+  LoginStatus _loginStatus = LoginStatus.notSignIn;
+  String? _username, _password;
+  
+  final _formKey = GlobalKey<FormState>();
+  late LoginController controller;
+  var value;
+
+
+  _LoginState(){
+    this.controller = LoginController();
+  }
+
+  signOut() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      preferences.setInt("value", 0);
+      _loginStatus = LoginStatus.notSignIn;
+    });
+  }
+  
+  getPref() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      value = preferences.getInt("value");
+      print(value);
+      _loginStatus = (value == 1) ? LoginStatus.notSignIn : LoginStatus.signIn;
+    });
+  }
+
+  _submit() async{
+
+    final form = _formKey.currentState;
+
+    if(form!.validate()){
+      form.save();
+      try{
+        User user = await controller.getLogin(_username!, _password!);
+        if (user.id != -1){
+          savePref(1, user.username, user.password);
+          _loginStatus = LoginStatus.signIn;
+        }else{
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Usuário não registrado!'))
+          );
+        }
+      }catch(e){
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.toString()))
+        );
+      }
+    }
+  }
+
+  savePref(int value, String user, String pass) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      preferences.setInt("value", value);
+      preferences.setString("user", user);
+      preferences.setString("pass", pass);
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getPref();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+  
+    switch (_loginStatus) {
+      case LoginStatus.notSignIn:
+        return Scaffold(
+            appBar: AppBar(
+              title: Text("Login Page"),
+            ),
+            body: Container(
+              child: Center(
+                child: Column(
+                  children: [
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: TextFormField(
+                              onSaved: (newValue) => _username = newValue,
+                              decoration: InputDecoration(
+                                labelText: "Username",
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: TextFormField(
+                              onSaved: (newValue) => _password = newValue,
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                                border: UnderlineInputBorder(),
+                              ),
+                            ),
+                          )
+                        ]
+                      ,)
+                    ),
+                    ElevatedButton(
+                      onPressed: _submit, 
+                      child: Text("Login"),
+                      
+                    )
+                  
+                  ],
+                ),
+              ),
+            ),
+          );
+        case LoginStatus.signIn:
+          return DashBoard();
+    }
+    
+  }
+
+}
